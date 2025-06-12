08e485cff08c6235ca01a2b72d39094820fd9a98
amit <wbamitkundu@gmail.com>
2025-06-12 07:53:04 +0530

    add html part 1 image path

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -43,7 +43,7 @@ function Home() {
 
             <div class="hero-header overflow-hidden px-5">
                 <div class="rotate-img">
-                    <img src="img/sty-1.png" class="img-fluid w-100" alt=""/>
+                    <img src="src/img/sty-1.png" class="img-fluid w-100" alt=""/>
                     <div class="rotate-sty-2"></div>
                 </div>
                 <div class="row gy-5 align-items-center">
@@ -53,7 +53,7 @@ function Home() {
                         <a href="#" class="btn btn-primary rounded-pill py-3 px-5 wow fadeInUp" data-wow-delay="0.7s">Get Started</a>
                     </div>
                     <div class="col-lg-6 wow fadeInRight" data-wow-delay="0.2s">
-                        <img src="img/hero-img-1.png" class="img-fluid w-100 h-100" alt=""/>
+                        <img src="src/img/hero-img-1.png" class="img-fluid w-100 h-100" alt=""/>
                     </div>
                 </div>
             </div>
