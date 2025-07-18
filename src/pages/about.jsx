ca6d0161480da07cf85a1b699ac4999d623a17bb
amit@aslwallets.in <amit@aslwallets.in>
2025-07-18 18:32:55 +0530

    seconf  phase api call and submit

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -41,7 +41,10 @@ return <>
 
 
 function About() {
-  //const [name, setName] = useState('');
+
+
+
+  
   const [userRegID, setUserRegID] = useState('');
   const [type, setType] = useState('');
   const [amount, setAmount] = useState('');
@@ -55,10 +58,32 @@ function About() {
   const [responseData, setResponseData] = useState(null);
   const [error, setError] = useState(null);
 
+  // const [users, setUsers] = useState([]);       // Store fetched data
+  // const [loading, setLoading] = useState(true); // Track loading state
+
+  // useEffect(() => {
+  //   //fetch('https://testproject.co.in/api/apiforapp/v1/wallet/add-money') // Sample API
+  //   fetch('https://jsonplaceholder.typicode.com/users') // Sample API
+  //     .then(response => {
+  //       if (!response.ok) {
+  //         throw new Error('Network response was not OK');
+  //       }
+  //       return response.json();
+  //     })
+  //     .then(data => {
+  //       setUsers(data);
+  //       setLoading(false);
+  //     })
+  //     .catch(error => {
+  //       setError(error.message);
+  //       setLoading(false);
+  //     });
+  // }, []);
+
+
   const handleSubmit = async (e) => {
     e.preventDefault();
     const token = '25|9QLLMryDLgU5ZHxCLvlbLAxwY5zuoyBhZM1Jn0mjb5671de4'; 
-    console.log(userRegID);
     try {
       const response = await fetch('https://testproject.co.in/api/apiforapp/v1/wallet/add-money', {
         method: 'POST',
@@ -80,8 +105,6 @@ function About() {
         }),
       });
       
-      console.log(response);
-
       if (!response.ok) {
         throw new Error('Failed to send data');
       }
@@ -96,41 +119,75 @@ function About() {
   return (
     <>
     <div className="container-fluid header position-relative overflow-hidden p-0">
-      <div className="hero-header overflow-hidden px-5">
-        <div className="row gy-5 align-items-center">
-          <h4>Post Data Example</h4>
-          <div className="col-lg-12">
+      <div className="hero-header overflow-hidden px-5">          
             <form onSubmit={handleSubmit}>
+              <div className="row gy-5 align-items-center">
+                <h4>Post Data Example</h4>
               <div className="col-lg-6 wow fadeInLeft">
-                <input className="form-control" type="text" value={userRegID} placeholder="Enter Reg no" onChange={(e) => setUserRegID(e.target.value)}/>
-                <select value={type} className="form-control">
-                  <option value=""> Select </option>
+                <input className="form-control mb-2" type="text" value={userRegID} placeholder="Enter Reg no" onChange={(e) => setUserRegID(e.target.value)}/>
+                <select value={type} className="form-control mb-2" onChange={(e) => setType(e.target.value)}>
+                  <option value=""> Select type </option>
                   <option value="online">Online</option>
                   <option value="offline">Offline</option>
                 </select>
-                <input className="form-control" type="text" value={type} placeholder="Enter Type" onChange={(e) => setName(e.target.value)}/>
-                <input className="form-control" type="text" value={name} placeholder="Enter title" onChange={(e) => setName(e.target.value)}/>
+                <input className="form-control mb-2" type="text" value={amount} placeholder="Enter amount" onChange={(e) => setAmount(e.target.value)}/>
+                <input className="form-control mb-2" type="text" value={requestForm} placeholder="Enter Request Form" onChange={(e) => setRequestForm(e.target.value)}/>
               </div>
               <div className="col-lg-6 wow fadeInRight">
-                <input className="form-control"type="text" value={name} placeholder="Enter title" onChange={(e) => setName(e.target.value)}/>
-                <input className="form-control" type="text" value={name} placeholder="Enter title" onChange={(e) => setName(e.target.value)}/>
-                <input className="form-control"type="text" value={name} placeholder="Enter title" onChange={(e) => setName(e.target.value)}/>
-                <input className="form-control" type="text" value={name} placeholder="Enter title" onChange={(e) => setName(e.target.value)}/>
-                <input className="form-control" type="text" value={name} placeholder="Enter title" onChange={(e) => setName(e.target.value)}/>
+                <input className="form-control mb-2"type="text" value={bankName} placeholder="Enter bank name" onChange={(e) => setBankName(e.target.value)}/>
+                <input className="form-control mb-2" type="text" value={transactionID} placeholder="Enter transaction ID" onChange={(e) => setTransactionID(e.target.value)}/>
+                <input className="form-control mb-2"type="text" value={paymentBy} placeholder="Enter payment by" onChange={(e) => setPaymentBy(e.target.value)}/>
+                <input className="form-control mb-2" type="text" value={depositDate} placeholder="Enter deposit date" onChange={(e) => setDepositDate(e.target.value)}/>
+                <input className="form-control mb-2" type="text" value={remarks} placeholder="Enter remarks" onChange={(e) => setRemarks(e.target.value)}/>
               </div>
-              <button type="submit">Send</button>
+              <button type="submit" className='btn btn-primary mb-4'>Send</button>
+              {/* <div className="">
+              <h4>User List from api</h4>
+              <ul>
+                {users.map(user => (
+                  <li key={user.id}>
+                    <strong>{user.name}</strong> - {user.email}
+                  </li>
+                ))}
+              </ul>
+              </div> */}
+              </div>
             </form>
-          </div>
+
+            
 
           {error && <p style={{ color: 'red' }}>{error}</p>}
 
           {responseData && (
-            <div>
+            <>
               <h3>Response:</h3>
-              <pre>{JSON.stringify(responseData, null, 2)}</pre>
-            </div>
+              {/* <pre>{JSON.stringify(responseData, null, 2)}</pre> */}
+            
+              <table className="table">
+                <thead>
+                  <tr>
+                    <th>Key</th>
+                    <th>Value</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {Object.entries(responseData.data).map(([key, value]) => (
+                    <tr key={key}>
+                      <td>{key}</td>
+                      <td>{String(value)}</td>
+
+                      {/* {Object.entries(value).map(([key, tt]) => (
+                        <td>{String(tt)}</td>
+                      ))} */}
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </>
           )}
-        </div>
+
+
+          
       </div>
     </div>
     </>
